c974c0f8f527b8bf8af1e2fe187191d67aca4a6b
Siwoon Lim <59671717+SWL-01@users.noreply.github.com>
2023-10-14 11:53:07 -0700

    added sql function

--- server.js
+++ server.js
@@ -3,60 +3,48 @@ const mysql = require("mysql2");
 const fs = require("fs");
 const url = require("url");
 
-let errorMessages = ["Error inserting data into the database", "Not found", "Internal Server Error"];
+let errorMessages = [
+  "Error inserting data into the database",
+  "Not found",
+  "Internal Server Error",
+  "Failed to execute query",
+];
 let successMessages = ["Data inserted successfully!"];
 
-const { parse } = require("querystring");
 const port = 3000;
 
 const connection = mysql.createConnection({
-  host: "localhost",
-
-  user: "root",
-
-  password: null,
-
-  database: "Comp4537_lab5",
+  host: "sql3.freesqldatabase.com",
+  user: "sql3653387",
+  password: "sdvrkkxkNK",
+  database: "sql3653387",
 });
 
 connection.connect((err) => {
   if (err) {
     console.error("Error connecting to database:", err);
-
     return;
   }
 
   console.log("Connected to the database!");
 
-  // Create table if not exists
-
   const createTableQuery = `
-
 CREATE TABLE IF NOT EXISTS patient (
-
-id INT AUTO_INCREMENT PRIMARY KEY,
-
-name VARCHAR(255) NOT NULL,
-
-age INT NOT NULL,
-
-diagnosis VARCHAR(255)
-
+id INT(11) AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(100) NOT NULL,
+dateOfBirth DATETIME NOT NULL,
 ) ENGINE=InnoDB;
-
 `;
 
   connection.query(createTableQuery, (err, result) => {
     if (err) throw err;
-
     console.log("Table is already created.");
   });
 
-  $query = "Select * from patient";
+  const query = "Select * from patient";
 
-  connection.query($query, (err, rows, result) => {
+  connection.query(query, (err, rows) => {
     if (err) throw err;
-
     console.log(successMessages[0], rows);
   });
 });
@@ -66,7 +54,7 @@ http
     const parsedUrl = url.parse(req.url, true);
     const headers = {
       "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*", // to allow cross-origin requests
+      "Access-Control-Allow-Origin": "*",
       "Access-Control-Allow-Methods": "GET, POST",
     };
 
@@ -74,29 +62,60 @@ http
       fs.readFile("index.html", "utf8", (err, data) => {
         if (err) {
           res.writeHead(500, { "Content-Type": "text/plain" });
-          res.end(ErrorMessages[2]);
+          res.end(errorMessages[2]);
         } else {
           res.writeHead(200, { "Content-Type": "text/html" });
           res.end(data);
         }
       });
     } else if (parsedUrl.pathname === "/insertData" && req.method === "POST") {
-      const insertQuery = "INSERT INTO patient (name, dateOfBirth) VALUES ('Sara Brown', '1901-01-01'), ('John Smith', '1941-01-01'), ('Jack Ma', '1961-01-30'), ('Elon Musk', '1999-01-01')";
+      const insertQuery =
+        "INSERT INTO patient (name, age, diagnosis) VALUES ('Sara Brown', 23, 'Healthy'), ('John Smith', 83, 'Healthy'), ('Jack Ma', 62, 'Healthy'), ('Elon Musk', 24, 'Healthy')";
 
-      connection.query(insertQuery, (err, results, rows) => {
+      connection.query(insertQuery, (err, results) => {
         if (err) {
-          res.writeHead(500, { "Content-Type": "application/json" });
+          res.writeHead(500, headers);
           res.end(
             JSON.stringify({
-              message: errorMessages[0]
+              message: errorMessages[0],
             })
           );
         } else {
-          res.writeHead(200, { "Content-Type": "application/json" });
+          res.writeHead(200, headers);
           res.end(JSON.stringify({ message: successMessages[0] }));
-          console.log(successMessages[0], rows);
+          console.log(successMessages[0]);
         }
       });
+    } else if (parsedUrl.pathname === "/submitQuery") {
+      if (req.method === "POST") {
+        let body = "";
+        req.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+        req.on("end", () => {
+          const query = querystring.parse(body).query;
+          connection.query(query, (err, results) => {
+            if (err) {
+              res.writeHead(500, headers);
+              res.end(JSON.stringify({ error: errorMessages[3] }));
+            } else {
+              res.writeHead(200, headers);
+              res.end(JSON.stringify(results));
+            }
+          });
+        });
+      } else if (req.method === "GET") {
+        const query = parsedUrl.query.query;
+        connection.query(query, (err, results) => {
+          if (err) {
+            res.writeHead(500, headers);
+            res.end(JSON.stringify({ error: errorMessages[3] }));
+          } else {
+            res.writeHead(200, headers);
+            res.end(JSON.stringify(results));
+          }
+        });
+      }
     } else {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end(errorMessages[1]);

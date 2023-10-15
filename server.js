291cb441e3b122bace36e451b0bd7ef56bb9254f
Siwoon Lim <59671717+SWL-01@users.noreply.github.com>
2023-10-15 02:26:13 -0700

    uncomment function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const mysql = require("mysql2");
 const fs = require("fs");
 const url = require("url");
-const querystring = require('querystring');
+const querystring = require("querystring");
 
 let errorMessages = [
   "Error inserting data into the database",
@@ -60,104 +60,104 @@ http
     };
 
     if (parsedUrl.pathname === "/insertData" && req.method === "POST") {
-      const insertQuery = "INSERT INTO patients (name, dateOfBirth) VALUES ('Sara Brown', '1901-01-01'), ('John Smith', '1941-01-01'), ('Jack Ma', '1961-01-30'), ('Elon Musk', '1999-01-01')";
+      const insertQuery =
+        "INSERT INTO patients (name, dateOfBirth) VALUES ('Sara Brown', '1901-01-01'), ('John Smith', '1941-01-01'), ('Jack Ma', '1961-01-30'), ('Elon Musk', '1999-01-01')";
       connection.query(insertQuery, (err, results) => {
-          if (err) {
-              res.writeHead(500, headers);
-              res.end(JSON.stringify({ error: errorMessages[0] }));
-          } else {
-              res.writeHead(200, headers);
-              res.end(JSON.stringify({ message: successMessages[0] }));
-          }
+        if (err) {
+          res.writeHead(500, headers);
+          res.end(JSON.stringify({ error: errorMessages[0] }));
+        } else {
+          res.writeHead(200, headers);
+          res.end(JSON.stringify({ message: successMessages[0] }));
+        }
       });
     } else if (parsedUrl.pathname === "/submitQuery" && req.method === "POST") {
       let body = "";
       req.on("data", (chunk) => {
-          body += chunk.toString();
+        body += chunk.toString();
       });
       req.on("end", () => {
-          const userQuery = querystring.parse(body).query.trim();
+        const userQuery = querystring.parse(body).query.trim();
 
-          // Check query type
-          if (userQuery.toUpperCase().startsWith('INSERT') || userQuery.toUpperCase().startsWith('SELECT')) {
-              connection.query(userQuery, (err, results) => {
-                  if (err) {
-                      res.writeHead(500, headers);
-                      res.end(JSON.stringify({ error: errorMessages[3] }));
-                  } else if (results.length === 0) {
-                      res.writeHead(404, headers);
-                      res.end(JSON.stringify({ error: errorMessages[1] }));
-                  } else {
-                      res.writeHead(200, headers);
-                      res.end(JSON.stringify(results));
-                  }
-              });
-          } else {
-              res.writeHead(400, headers); // Bad Request for unsupported query types
-              res.end(JSON.stringify({ error: "Unsupported query type." }));
-          }
+        // Check query type
+        if (
+          userQuery.toUpperCase().startsWith("INSERT") ||
+          userQuery.toUpperCase().startsWith("SELECT")
+        ) {
+          connection.query(userQuery, (err, results) => {
+            if (err) {
+              res.writeHead(500, headers);
+              res.end(JSON.stringify({ error: errorMessages[3] }));
+            } else if (results.length === 0) {
+              res.writeHead(404, headers);
+              res.end(JSON.stringify({ error: errorMessages[1] }));
+            } else {
+              res.writeHead(200, headers);
+              res.end(JSON.stringify(results));
+            }
+          });
+        } else {
+          res.writeHead(400, headers); // Bad Request for unsupported query types
+          res.end(JSON.stringify({ error: "Unsupported query type." }));
+        }
       });
-  } else {
+    } else {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end(errorMessages[1]);
-  }
+    }
 
+    if (parsedUrl.pathname === "/insertData" && req.method === "POST") {
+      const insertQuery =
+        "INSERT INTO patients (name, dateOfBirth) VALUES ('Sara Brown', '1901-01-01'), ('John Smith', '1941-01-01'), ('Jack Ma', '1961-01-30'), ('Elon Musk', '1999-01-01')";
 
-
-
-
-    // if (parsedUrl.pathname === "/insertData" && req.method === "POST") {
-    //   const insertQuery =
-    //     "INSERT INTO patients (name, dateOfBirth) VALUES ('Sara Brown', '1901-01-01'), ('John Smith', '1941-01-01'), ('Jack Ma', '1961-01-30'), ('Elon Musk', '1999-01-01')";
-
-    //   connection.query(insertQuery, (err, results) => {
-    //     if (err) {
-    //       res.writeHead(500, headers);
-    //       res.end(
-    //         JSON.stringify({
-    //           message: errorMessages[0],
-    //         })
-    //       );
-    //     } else {
-    //       res.writeHead(200, headers);
-    //       res.end(JSON.stringify({ message: successMessages[0] }));
-    //       console.log(successMessages[0]);
-    //     }
-    //   });
-    // } else if (parsedUrl.pathname === "/submitQuery") {
-    //   if (req.method === "POST") {
-    //     let body = "";
-    //     req.on("data", (chunk) => {
-    //       body += chunk.toString();
-    //     });
-    //     req.on("end", () => {
-    //       const query = querystring.parse(body).query;
-    //       connection.query(query, (err, results) => {
-    //         if (err) {
-    //           res.writeHead(500, headers);
-    //           res.end(JSON.stringify({ error: errorMessages[3] }));
-    //         } else {
-    //           res.writeHead(200, headers);
-    //           res.end(JSON.stringify(results));
-    //         }
-    //       });
-    //     });
-    //   } else if (req.method === "GET") {
-    //     const query = parsedUrl.query.query;
-    //     connection.query(query, (err, results) => {
-    //       if (err) {
-    //         res.writeHead(500, headers);
-    //         res.end(JSON.stringify({ error: errorMessages[3] }));
-    //       } else {
-    //         res.writeHead(200, headers);
-    //         res.end(JSON.stringify(results));
-    //       }
-    //     });
-    //   }
-    // } else {
-    //   res.writeHead(404, { "Content-Type": "text/plain" });
-    //   res.end(errorMessages[1]);
-    // }
+      connection.query(insertQuery, (err, results) => {
+        if (err) {
+          res.writeHead(500, headers);
+          res.end(
+            JSON.stringify({
+              message: errorMessages[0],
+            })
+          );
+        } else {
+          res.writeHead(200, headers);
+          res.end(JSON.stringify({ message: successMessages[0] }));
+          console.log(successMessages[0]);
+        }
+      });
+    } else if (parsedUrl.pathname === "/submitQuery") {
+      if (req.method === "POST") {
+        let body = "";
+        req.on("data", (chunk) => {
+          body += chunk.toString();
+        });
+        req.on("end", () => {
+          const query = querystring.parse(body).query;
+          connection.query(query, (err, results) => {
+            if (err) {
+              res.writeHead(500, headers);
+              res.end(JSON.stringify({ error: errorMessages[3] }));
+            } else {
+              res.writeHead(200, headers);
+              res.end(JSON.stringify(results));
+            }
+          });
+        });
+      } else if (req.method === "GET") {
+        const query = parsedUrl.query.query;
+        connection.query(query, (err, results) => {
+          if (err) {
+            res.writeHead(500, headers);
+            res.end(JSON.stringify({ error: errorMessages[3] }));
+          } else {
+            res.writeHead(200, headers);
+            res.end(JSON.stringify(results));
+          }
+        });
+      }
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end(errorMessages[1]);
+    }
   })
   .listen(port, () => {
     console.log(`Server is running on port ${port}`);
